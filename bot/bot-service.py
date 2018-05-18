b232a24876338a9cd9991ef074574ff56d86d7c6
streamtv85 <streamtv85@gmail.com>
2018-05-18 19:04:15 +0300

    added methods to get tickers from Bitfinex and Exmo. Callback function now calculates the price difference and moving averages

--- bot/bot-service.py
+++ bot/bot-service.py
@@ -1,5 +1,8 @@
 import logging
 import configparser
+import os
+from os import path
+
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
@@ -28,9 +31,11 @@ logger.addHandler(ch)
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-price_diff_ma = 0.0
+price_diff_ma_slow = 0.0
+price_diff_ma_fast = 0.0
 price_diff_prev = 0.0
-price_ma_period = 60
+price_ma_period_slow = 20
+price_ma_period_fast = 3
 
 
 def add_message_handlers(disp):
@@ -76,25 +81,40 @@ add_command_handlers(dispatcher)
 
 
 def callback_exchanges_data(bot, job):
-    global price_diff_prev, price_diff_ma
+    global price_diff_prev, price_diff_ma_slow, price_diff_ma_fast
     text = ""
-    price_diff = round(get_bitfinex_btc_price() - get_exmo_btc_price(), 2)
+    price_diff = get_price_diff()
+
+    if price_diff_ma_slow == 0:
+        price_diff_ma_slow = price_diff
+    if price_diff_ma_fast == 0:
+        price_diff_ma_fast = price_diff
+    price_diff_ma_slow = (price_diff + price_ma_period_slow * price_diff_ma_slow) / (price_ma_period_slow + 1)
+    price_diff_ma_fast = (price_diff + price_ma_period_fast * price_diff_ma_fast) / (price_ma_period_fast + 1)
+
     # if price_diff * price_diff_prev < 0:
     # price difference changed sign
     # text = "Warning! Exmo - Bitfinex price difference has changed. Now it equals {0}, before it was {1}".format(
     #     price_diff, price_diff_prev)
 
-    text = "Bitfinex - Exmo price difference is {0} USD, before it was {1} USD. Average: {2}".format(price_diff,
-                                                                                                     price_diff_prev,
-                                                                                                     price_diff_ma)
-    list_of_chats = get_all_chats()
-    for chat in list_of_chats:
-        bot.send_message(chat_id=chat, text=text)
+    f = open("data.csv", "a+")
+    csv = "{0},{1},{2}\n".format(
+        price_diff,
+        round(price_diff_ma_slow, 2),
+        round(price_diff_ma_fast, 2))
+    text = "Bitfinex - Exmo price difference is {0} USD, before it was {1} USD. Slow MA: {2}, Fast MA: {3}".format(
+        price_diff,
+        price_diff_prev,
+        round(price_diff_ma_slow, 2),
+        round(price_diff_ma_fast, 2))
+    logger.info(text)
+    f.write(csv)
+    f.close()
+    # list_of_chats = get_all_chats()
+    # for chat in list_of_chats:
+    #     bot.send_message(chat_id=chat, text=text)
 
     price_diff_prev = price_diff
-    if price_diff_ma == 0:
-        price_diff_ma = price_diff_prev
-    price_diff_ma = (price_diff + price_ma_period * price_diff_ma) / (price_ma_period + 1)
 
 
 # if get_all_chats():
@@ -105,6 +125,8 @@ def callback_exchanges_data(bot, job):
 #         print(updater.bot.get_chat(chat[0]).get_member())
 
 logger.info("init regular job for execution")
+if path.exists("data.csv"):
+    os.remove("data.csv")
 job_minute = job_queue.run_repeating(callback_exchanges_data, interval=60, first=0)
 
 # polling loop
--- bot/exchange_ticker.py
+++ bot/exchange_ticker.py
@@ -1,7 +1,27 @@
+import math
+import random
+import time
+
 import requests
 import logging
 
 logger = logging.getLogger('bot-service.exchange')
+# time.clock()
+start = time.time()
+timestamp = 0.0
+
+
+def get_price_diff(mock=False):
+    global timestamp, start
+    if mock:
+        period = 300.0
+        amp = 300.0
+        timestamp = time.time() - start
+        logger.info("timestamp: {0}".format(timestamp))
+        return round(amp * math.sin(2 * math.pi * timestamp / period) + random.gauss(0, amp / 5), 2)
+        # return round(amp * math.sin(2 * math.pi * timestamp * period) + random.gauss(0, amp / 10), 2)
+
+    return round(get_bitfinex_btc_price() - get_exmo_btc_price(), 2)
 
 
 def get_exmo_btc_price():
@@ -22,3 +42,8 @@ def get_data_from_api(base_url, path):
 
 print("BTC price on Exmo: " + str(get_exmo_btc_price()) + " USD")
 print("BTC price on Bitfinex: " + str(get_bitfinex_btc_price()) + " USD")
+
+# step = 1  # seconds
+# while True:
+#     print(get_price_diff(mock=True))
+#     time.sleep(step)
