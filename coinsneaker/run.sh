bc626b5bdea8578d9da9624ff87bf3fc1e35519e
streamtv85 <streamtv85@gmail.com>
2018-05-25 13:08:25 +0300

    script to run python file in pipenv

---
+++ coinsneaker/run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+
+#SERVICE_DIR=/usr/local/bin/bot-service
+
+#DAEMON_NAME=bot-service
+
+
+#sudo chmod 755 $SERVICE_DIR/coinsneaker/bot_service.py
+
+#To start the program:
+pipenv run python ./bot_service.py
