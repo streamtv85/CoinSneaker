5bf85151ed75ac08de24e76ab9c9402fc42baddf
streamtv85 <streamtv85@gmail.com>
2018-06-07 16:43:54 +0300

    refactored jokes. More unique content

--- coinsneaker/events.py
+++ coinsneaker/events.py
@@ -9,6 +9,7 @@ import requests
 
 import telegram
 import urllib3
+from urllib.parse import urlencode
 
 from coinsneaker import dbmanager, graph
 
@@ -125,18 +126,25 @@ def master(bot, update):
 
 
 def joke(bot, update):
-    url = "http://umorili.herokuapp.com/api/random?num=100"
-    number = random.randint(0, 100)
-    # print(number)
+    sources = ['bash.im', 'anekdot.ru', 'det.org.ru']
+    url = "http://umorili.herokuapp.com/api/sources"
+    source_number = random.randint(0, len(sources) - 1)
     reply = ""
     response = requests.get(url)
+    if response.status_code == 200:
+        matches = [x for x in response.json() if x[0]['site'] == sources[source_number]]
+        if matches:
+            site = matches[0][0]['site']
+            name = matches[0][0]['name']
+        encoded_args = urlencode({'site': site, 'name': name, 'num': 100})
+        url = "http://umorili.herokuapp.com/api/get?" + encoded_args
+        response = requests.get(url)
     if response.status_code != 200:
         logger.warning(
             "Status code was {0} when accessing {1}".format(response.status_code, url))
         reply = "Эм... Боюсь вам эта шутка не понравится..."
     else:
-        if number > len(response.json()):
-            number = len(response.json())
+        number = random.randint(0, len(response.json()) - 1)
         reply = html.unescape(response.json()[number]['elementPureHtml'])
         reply = re.sub(r'<a.*/a>,*', '', str(reply))
         reply = re.sub(r'<[^<]+?>', '', str(reply))
